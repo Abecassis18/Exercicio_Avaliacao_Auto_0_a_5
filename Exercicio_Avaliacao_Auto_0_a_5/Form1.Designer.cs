a53556396095ceacdce69d4bfc5d4541ccdec362
Paulo Abecassis <abecassis_mp@outlook.com>
2022-10-12 02:31:44 +0000

    Exercicio finalizado e testado conforme a minha resolução baseado no livro

---
+++ Exercicio_Avaliacao_Auto_0_a_5/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Exercicio_Avaliacao_Auto_0_a_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.grpAvaliacao = new System.Windows.Forms.GroupBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lstNotas = new System.Windows.Forms.ListView();
+            this.Marca = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Modelo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Avaliação = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnCaalcular = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.grpAvaliacao.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Marca";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Modelo";
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(156, 16);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(100, 20);
+            this.txtMarca.TabIndex = 2;
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(156, 46);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(100, 20);
+            this.txtModelo.TabIndex = 3;
+            // 
+            // grpAvaliacao
+            // 
+            this.grpAvaliacao.Controls.Add(this.comboBox5);
+            this.grpAvaliacao.Controls.Add(this.comboBox4);
+            this.grpAvaliacao.Controls.Add(this.comboBox3);
+            this.grpAvaliacao.Controls.Add(this.comboBox2);
+            this.grpAvaliacao.Controls.Add(this.comboBox1);
+            this.grpAvaliacao.Location = new System.Drawing.Point(12, 107);
+            this.grpAvaliacao.Name = "grpAvaliacao";
+            this.grpAvaliacao.Size = new System.Drawing.Size(200, 201);
+            this.grpAvaliacao.TabIndex = 4;
+            this.grpAvaliacao.TabStop = false;
+            this.grpAvaliacao.Text = "Avaliações";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(130, 149);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(47, 21);
+            this.comboBox5.TabIndex = 4;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(130, 122);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(47, 21);
+            this.comboBox4.TabIndex = 3;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(130, 95);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(47, 21);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(130, 68);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(47, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(130, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(47, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // lstNotas
+            // 
+            this.lstNotas.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Marca,
+            this.Modelo,
+            this.Avaliação});
+            this.lstNotas.HideSelection = false;
+            this.lstNotas.Location = new System.Drawing.Point(230, 107);
+            this.lstNotas.Name = "lstNotas";
+            this.lstNotas.Size = new System.Drawing.Size(351, 201);
+            this.lstNotas.TabIndex = 5;
+            this.lstNotas.UseCompatibleStateImageBehavior = false;
+            this.lstNotas.View = System.Windows.Forms.View.Details;
+            // 
+            // Marca
+            // 
+            this.Marca.Text = "Marca";
+            this.Marca.Width = 120;
+            // 
+            // Modelo
+            // 
+            this.Modelo.Text = "Modelo";
+            this.Modelo.Width = 120;
+            // 
+            // Avaliação
+            // 
+            this.Avaliação.Text = "Avaliação";
+            this.Avaliação.Width = 120;
+            // 
+            // btnCaalcular
+            // 
+            this.btnCaalcular.Location = new System.Drawing.Point(302, 29);
+            this.btnCaalcular.Name = "btnCaalcular";
+            this.btnCaalcular.Size = new System.Drawing.Size(131, 23);
+            this.btnCaalcular.TabIndex = 6;
+            this.btnCaalcular.Text = "Calcular";
+            this.btnCaalcular.UseVisualStyleBackColor = true;
+            this.btnCaalcular.Click += new System.EventHandler(this.btnCaalcular_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(467, 29);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(93, 23);
+            this.btnFechar.TabIndex = 7;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 341);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnCaalcular);
+            this.Controls.Add(this.lstNotas);
+            this.Controls.Add(this.grpAvaliacao);
+            this.Controls.Add(this.txtModelo);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Programa de Avaliação";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpAvaliacao.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.GroupBox grpAvaliacao;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListView lstNotas;
+        private System.Windows.Forms.Button btnCaalcular;
+        private System.Windows.Forms.ColumnHeader Marca;
+        private System.Windows.Forms.ColumnHeader Modelo;
+        private System.Windows.Forms.ColumnHeader Avaliação;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ Exercicio_Avaliacao_Auto_0_a_5/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio_Avaliacao_Auto_0_a_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (object objeto in grpAvaliacao.Controls)
+            {
+                if (objeto is ComboBox)
+                {
+                    for (int cont = 1; cont <= 5; cont++)
+                    {
+                        ((ComboBox)objeto).Items.Add(cont);
+                    }
+                    ((ComboBox)objeto).Text = "1";
+                }
+            }
+        }
+        private string pontuacao()
+        {
+            int soma = 0;
+            string resultado = "";
+
+            foreach (Object objeto in grpAvaliacao.Controls)
+            {
+                if (objeto is ComboBox)
+                {
+                    for (int cont = 1; cont <= 5; cont++)
+                    {
+                        ((ComboBox)objeto).Items.Add(cont);
+                    }
+                    soma += int.Parse(((ComboBox)objeto).Text);
+
+                    if (soma <= 5) resultado = "Fraco";
+                    else if (soma <= 10) resultado = "Insuficiente";
+                    else if (soma <= 15) resultado = "Suficiente";
+                    else if (soma <= 20) resultado = "Bom";
+                    else if (soma <= 25) resultado = "Muito Bom";
+                    else if (soma <= 30) resultado = "Excelente";
+                }
+            }
+            return resultado;
+        }
+
+        private void fechar()
+        {
+            this.Close();
+        }
+        private void btnCaalcular_Click(object sender, EventArgs e)
+        {
+            if (txtMarca.Text != "" && txtModelo.Text != "")
+            {
+                string marca = txtMarca.Text;
+                string modelo = txtModelo.Text;
+                string resultado = pontuacao();
+
+                ListViewItem Novo = new ListViewItem(marca.ToString());//Tem que ser aqui a setar o primeiro item pq se não ele desconfigura os outrso itens da listview.
+
+                Novo.SubItems.Add(modelo.ToString());
+                Novo.SubItems.Add(resultado.ToString());
+
+                lstNotas.Items.Add(Novo);
+
+                txtMarca.Focus();
+                txtMarca.ResetText();
+                txtModelo.ResetText();
+
+                
+            }
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            fechar();
+        }
+    }
+}
